e2df4afa06739689931caa39cddd0103bc3b67bc
RomanKoshkin-qa <oksana.oksana.oksana9@yandex.ru>
2025-07-16 17:22:34 +0300

    Add functions and tests for cart + product

---
+++ test_shop.py
@@ -0,0 +1,80 @@
+"""
+Протестируйте классы из модуля homework/models.py
+"""
+import pytest
+
+from models import Product,Cart
+
+
+@pytest.fixture
+def product():
+    return Product("book", 100, "This is a book", 1000)
+
+
+class TestProducts:
+    """
+    Тестовый класс - это способ группировки ваших тестов по какой-то тематике
+    Например, текущий класс группирует тесты на класс Product
+    """
+
+    def test_product_check_quantity(self, product):
+        #  напишите проверки на метод check_quantity
+        assert product.check_quantity(100) is True
+        assert product.check_quantity(1001) is False
+        assert product.check_quantity(999) is True
+
+    def test_product_buy(self, product):
+        #  напишите проверки на метод buy
+        product.buy(200)
+        assert product.quantity == 800
+
+    def test_product_buy_more_than_available(self, product):
+        #  напишите проверки на метод buy,
+        #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
+        with pytest.raises(ValueError):
+            product.buy(1200)
+
+class TestCart:
+    """
+     Напишите тесты на методы класса Cart
+        На каждый метод у вас должен получиться отдельный тест
+        На некоторые методы у вас может быть несколько тестов.
+        Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
+    """
+    def test_add_product(self, product):
+        cart = Cart()
+        cart.add_product(product, 4)
+        assert cart.products[product] == 4
+    def test_remove_product(self, product):
+        cart = Cart()
+        cart.add_product(product, 4)
+        cart.remove_product(product, 1)
+        cart2 = Cart()
+        cart2.add_product(product, 4)
+        cart2.remove_product(product, 6)
+        assert cart2.products[product] == 0
+        cart3 = Cart()
+        cart3.add_product(product, 4)
+        cart3.remove_product(product)
+        assert cart3.products[product] == 0
+    def test_clear(self, product):
+        cart = Cart()
+        cart.add_product(product, 4)
+        cart.clear()
+        assert cart.products == {}
+    def test_get_total_price(self,product):
+        cart = Cart()
+        cart.add_product(product, 4)
+        total = cart.get_total_price()
+        assert total == 400
+
+    def test_buy(self,product):
+        cart = Cart()
+        cart.add_product(product, 1001)
+        with pytest.raises(ValueError):
+            cart.buy()
+        cart2 = Cart()
+        cart2.add_product(product, 3)
+        cart2.buy()
+        assert cart2.products== {}
+        assert product.quantity == 997
